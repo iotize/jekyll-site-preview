8ef78d182c6c201e50c6ba6973ab3b1dc899e9b1
Ryan Davies <me@ryanjdavies.com>
2014-05-09 13:42:45 +0100

    Strip out debug lines

--- app.rb
+++ app.rb
@@ -4,14 +4,6 @@ require 'fileutils'
 require 'digest'
 require 'jekyll'
 
-require 'logger'
-
-enable :logging
-
-before do
-  logger.level = Logger::DEBUG
-end
-
 def post_html(post_title, post_content)
   # Generate a unique hash from the content
   hash = Digest::SHA1.hexdigest(post_content)
@@ -23,19 +15,11 @@ def post_html(post_title, post_content)
   posts_dir = File.join(site_dir, '_posts')
   build_dir = File.join(destination_dir, 'build')
   
-  logger.debug "temp dir: #{temp_dir}"
-  logger.debug "dest dir: #{destination_dir}"
-  logger.debug "site dir: #{site_dir}"
-  logger.debug "posts dir: #{posts_dir}"
-  logger.debug "build dir: #{build_dir}"
-
   # Generate post path
   post_title = "Example"
   post_date = Date.today
   post_name = [ post_date.strftime("%Y-%m-%d"), post_title.downcase ].join('-') + '.md'
   post_path = File.join(posts_dir, post_name)
-  
-  logger.debug "post path: #{post_path}"
 
   # Create the posts directory
   FileUtils::mkdir_p(posts_dir)
@@ -63,25 +47,19 @@ def post_html(post_title, post_content)
 
   # Figure out the compiled post path
   compiled_post_path = File.join(build_dir, post_title.downcase) + '.html'
-  
-  puts "compiled post path: #{compiled_post_path}"
 
   # Get HTML contents
   compiled_post_contents = File.read(compiled_post_path)
   
-  # # Delete directory
-  # FileUtils.rm_rf(destination_dir)
+  # Delete directory
+  FileUtils.rm_rf(destination_dir)
   
   # Return post HTML contents
   compiled_post_contents
 end
 
 get '/' do
-  "Hello." + "<br/>Root: #{settings.root}"
-end
-
-get '/test' do
-  post_html 'My First Post', 'Hello world!'
+  "Hello."
 end
 
 post '/build' do
