c6a90f6228daf300b66b7f6cb5b16153f92a2a42
Ryan Davies <me@ryanjdavies.com>
2014-05-09 12:57:25 +0100

    Initial commit of working prototype

---
+++ Gemfile
@@ -0,0 +1,5 @@
+ruby '2.1.1'
+
+gem 'sinatra', '1.4.5'
+gem 'jekyll', '2.0.3'
+
---
+++ app.rb
@@ -0,0 +1,58 @@
+require 'date'
+require 'fileutils'
+require 'digest'
+require 'sinatra'
+require 'jekyll'
+
+def post_html(post_title, post_content)
+  # Generate a unique hash from the content
+  hash = Digest::SHA1.hexdigest(post_content)
+
+  # Calculate directory paths
+  temp_dir = "tmp"
+  destination_dir = File.join(temp_dir, hash)
+  site_dir = File.join(destination_dir, 'site')
+  posts_dir = File.join(site_dir, '_posts')
+  build_dir = File.join(destination_dir, 'build')
+
+  # Generate post path
+  post_title = "Example"
+  post_date = Date.today
+  post_name = [ post_date.strftime("%Y-%m-%d"), post_title.downcase ].join('-') + '.md'
+  post_path = File.join(posts_dir, post_name)
+
+  # Create the posts directory
+  FileUtils::mkdir_p(posts_dir)
+
+  # Add a _config.yml file
+  file = File.open(File.join(site_dir, '_config.yml'), "w") do |f|
+    f.puts('markdown: kramdown')
+    f.puts('permalink: none')
+  end
+
+  # Add a post with today's date
+  File.open(post_path, "w") do |f|
+    f.write(post_content)
+  end
+
+  # Build it with Jekyll
+  options = Jekyll::configuration({ 'source' => site_dir, 'destination' => build_dir, safe: true })
+  site = Jekyll::Site.new(options)
+  site.process
+
+  # Figure out the compiled post path
+  compiled_post_path = File.join(build_dir, post_title) + '.html'
+
+  # Get HTML contents
+  compiled_post_contents = File.read(compiled_post_path)
+  
+  # Delete directory
+  FileUtils.rm_rf(destination_dir)
+  
+  # Return post HTML contents
+  compiled_post_contents
+end
+
+post '/build' do
+  post_html params[:title], params[:content]
+end
